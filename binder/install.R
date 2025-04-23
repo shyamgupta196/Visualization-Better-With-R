57b44482eb470f97e788cbc8ceb8a84910fa3524
shyam gupta <60971003+shyamgupta196@users.noreply.github.com>
2025-04-23 12:48:16 +0200

    Update install.R

---
+++ binder/install.R
@@ -0,0 +1,10 @@
+# install.R
+
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("forcats")
+install.packages("hrbrthemes")
+install.packages("viridis")
+install.packages("zoo")
+install.packages("corrplot")
+install.packages("maps")
